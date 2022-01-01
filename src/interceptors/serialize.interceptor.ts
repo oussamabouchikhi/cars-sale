4eaa239ee569c9a9096af4eedd02edf5275bb119
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2022-01-01 13:59:55 +0100

    ♻️refactor  custom serializer code

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,10 +1,15 @@
 import { 
   CallHandler, 
   ExecutionContext, 
-  NestInterceptor 
+  NestInterceptor, 
+  UseInterceptors
 } from "@nestjs/common";
 import { plainToInstance } from "class-transformer";
 import { map, Observable } from "rxjs";
+
+export function Serialize(dto: any) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
 
 export class SerializeInterceptor implements NestInterceptor {
   constructor(private dto: any) {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,10 +8,9 @@ import {
   Param,
   Patch,
   Post,
-  Query,
-  UseInterceptors 
+  Query
 } from '@nestjs/common';
-import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
@@ -26,7 +25,7 @@ export class UsersController {
     return this.usersService.create(body.email, body.password);
   }
 
-  @UseInterceptors(new SerializeInterceptor(UserDto))
+  @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
