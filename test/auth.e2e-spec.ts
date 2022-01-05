53b5924f2f36ee3651b1fc78c157c5c12bbdfdb4
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2022-01-05 17:16:56 +0100

    🧪 signup the get loged in user user

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -27,4 +27,21 @@ describe('Authentication system', () => {
         expect(email).toEqual(tmpEmail);
       });
   });
+
+  it('should signup a new user then get the currently logged in user', async () => {
+    const email = 'test@asdf.com';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
