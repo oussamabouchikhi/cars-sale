75721329da036196aea8aabbb67a243d63d40e28
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2022-01-06 14:01:16 +0100

    ✨ add create report feature

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,30 @@
+import { IsLatitude, IsLongitude, IsNumber, IsString, Max, Min } from "class-validator";
+
+export class CreateReportDto {
+	@IsString()
+  make: string;
+
+  @IsString()
+	model: string;
+
+  @IsNumber()
+  @Min(1950)
+  @Max(2050)
+	year: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+	mileage: number;
+
+  @IsLongitude()
+	lng: number;
+
+  @IsLatitude()
+	lat: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+	price: number; 
+}
