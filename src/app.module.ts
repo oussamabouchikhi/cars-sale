a3e97241a5c4632c14df2f9760da9b332f012c9d
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2022-01-08 17:51:31 +0100

    :wrench: Set cookie key via environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,11 +43,14 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(
+    private configService: ConfigService
+  ) {}
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['lfjvndflbv'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');
